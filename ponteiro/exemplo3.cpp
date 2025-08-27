65eebc37f975db42a5f73a909fe7bb80434fc8eb
JoaoGeraldo-dev <joaogeraldobussolo@gmail.com>
2025-08-27 09:07:15 -0300

    27/08

---
+++ ponteiro/exemplo3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int a;
+	int *ptr;
+	
+	a = 90;
+	ptr = &a;
+	
+	cout << "a = " << a << endl;
+	cout << "Endereco de 'a' = " << &a << endl;
+	
+	cout<< "ptr = " << ptr << endl;
+	cout << "*ptr = " << *ptr << endl;
+	
+	*ptr = 120;
+	
+	cout << "a = " << a << endl;
+	cout << "ptr = " << ptr << endl;
+	cout << "*ptr = " << *ptr << endl;
+	
+	
+	
+	
+	
+	
+}
